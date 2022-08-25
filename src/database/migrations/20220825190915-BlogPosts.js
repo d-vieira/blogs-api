a4ee277fee2eac38bf4bd44addc98efec687eea8
Daniel Vieira <dann-vieira@hotmail.com>
2022-08-25 16:29:08 -0300

    Cria migration para a entidade BlogPosts

---
+++ src/database/migrations/20220825190915-BlogPosts.js
@@ -0,0 +1,61 @@
+'use strict';
+
+module.exports = {
+  /**
+    * @param {import('sequelize').QueryInterface} queryInterface
+    * @param {import('sequelize').Sequelize } Sequelize
+  */
+
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('BlogPosts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      content: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      published: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated: {
+        allowNull: false,
+        type: Sequelize.DATE 
+      },
+      userId: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Users',
+          key: 'id',
+        },
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+     await queryInterface.dropTable('BlogPosts')
+  },
+};
