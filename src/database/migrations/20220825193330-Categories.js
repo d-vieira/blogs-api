d43b81585c11511258ccd1836ec53fcbfd8a9f4c
Daniel Vieira <dann-vieira@hotmail.com>
2022-08-25 17:14:59 -0300

    fix: remove comentarios

--- src/database/migrations/20220825193330-Categories.js
+++ src/database/migrations/20220825193330-Categories.js
@@ -7,12 +7,6 @@ module.exports = {
   */
 
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
     await queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
@@ -28,12 +22,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
     await queryInterface.dropTable('Categories');
   },
 };
