fb948f8048ead6636f64fcf1ed5b241144a63921
Daniel Vieira <dann-vieira@hotmail.com>
2022-08-25 16:55:00 -0300

    feat: Cria migration para a entidade PostCategories

---
+++ src/database/migrations/20220825194738-PostCategories.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = {
+  /**
+    * @param {import('sequelize').QueryInterface} queryInterface
+    * @param {import('sequelize').Sequelize } Sequelize
+  */
+
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('PostCategories', {
+      postId: {
+        primaryKey: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'BlogPosts',
+          key: 'id',
+        },
+      },
+      categoryId: {
+        primaryKey: true,
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        references: {
+          model: 'Categories',
+          key: 'id',
+        },
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('PostCategories');
+  },
+};
