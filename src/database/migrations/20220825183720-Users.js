5fc946bbefbfae029fd2625009695ba2e9c3c031
Daniel Vieira <dann-vieira@hotmail.com>
2022-08-25 16:08:10 -0300

    feat: Cria migration para a entidade Users

---
+++ src/database/migrations/20220825183720-Users.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const sequelize = require('sequelize');
+
+module.exports = {
+/**
+  * @param {import('sequelize').QueryInterface} queryInterface
+  * @param {import('sequelize').Sequelize } Sequelize
+*/
+
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      displayName: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.STRING
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('Users');
+  }
+};
