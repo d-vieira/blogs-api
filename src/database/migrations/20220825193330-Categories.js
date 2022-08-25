626045c9fd24f9fe837189a6482282410ce38bf0
Daniel Vieira <dann-vieira@hotmail.com>
2022-08-25 16:42:52 -0300

    feat: Cria migration para a entidade Categories

---
+++ src/database/migrations/20220825193330-Categories.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  /**
+    * @param {import('sequelize').QueryInterface} queryInterface
+    * @param {import('sequelize').Sequelize } Sequelize
+  */
+
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createDatabase('Categories', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('Categories');
+  },
+};
