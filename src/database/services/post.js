bb9d0f4726e333b4b7328569c7194eb81050ee52
Daniel Vieira <dann-vieira@hotmail.com>
2022-08-27 23:38:15 -0300

    refact: Inclui as infos sobre o User autor do post e categoria do post

--- src/database/services/post.js
+++ src/database/services/post.js
@@ -1,7 +1,21 @@
-const { BlogPost } = require('../models');
+const { BlogPost, User, Category } = require('../models');
 
 const findAll = async () => {
-  const data = await BlogPost.findAll();
+  const data = await BlogPost.findAll({
+    include: [
+      {
+        model: User,
+        as: 'user',
+        attributes: {
+          exclude: ['password'],
+        },
+      },
+      {
+        model: Category,
+        as: 'categories',
+      },
+    ],
+  });
   if (!data) return { code: 404, message: 'Not Found' };
   return { code: 200, data };
 };
